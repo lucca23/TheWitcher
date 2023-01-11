e9ba6f176e7643facc122c71c9f8974062fad382
Lucca <luccamanfredotti@gmail.com>
2023-01-11 12:39:42 +0100

    añadidos atributos

--- src/Witcher/thewitcher.java
+++ src/Witcher/thewitcher.java
@@ -1,6 +1,9 @@
 package Witcher;
 
 public class thewitcher {
+	private String nombre;
+	private int salud;
+	private boolean tienealma;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
