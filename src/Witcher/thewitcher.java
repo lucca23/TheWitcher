4c20198f9dd3a324597b6fbf0f845ced63f94a8c
Lucca <luccamanfredotti@gmail.com>
2023-01-11 13:40:02 +0100

    metodo isDeath

--- src/Witcher/thewitcher.java
+++ src/Witcher/thewitcher.java
@@ -39,11 +39,29 @@ public class thewitcher {
 		this.tienealma = tienealma;
 	}
 
-
+	public boolean isDeath() {
+		if(salud==0) {
+			return true;
+		} else if (!tienealma) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		thewitcher brujo = new thewitcher();
+		brujo.setNombre("Juan D");
+		brujo.setSalud(100);
+		brujo.setTienealma(true);
+		
+		if (brujo.isDeath()) {
+			System.out.println(brujo.getNombre() + " esta muerto.");
+		} else {
+			System.out.println(brujo.getNombre() + " esta vivo.");
+		}
 	}
 
 }
