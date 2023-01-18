67c4e7f9a593458edc1c05aade3603963f4fcf28
Ismael <ismaelbd042@gmail.com>
2023-01-18 14:02:29 +0100

    Añadir metodo revieveDamage

--- src/Witcher/thewitcher.java
+++ src/Witcher/thewitcher.java
@@ -4,43 +4,33 @@ public class thewitcher {
 	private String nombre;
 	private int salud;
 	private boolean tienealma;
-	
+
 	public String getNombre() {
 		return nombre;
 	}
-
-
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
-
-
 	public int getSalud() {
 		return salud;
 	}
-
-
 
 	public void setSalud(int salud) {
 		this.salud = salud;
 	}
 
-
-
 	public boolean isTienealma() {
 		return tienealma;
 	}
-
-
 
 	public void setTienealma(boolean tienealma) {
 		this.tienealma = tienealma;
 	}
 
 	public boolean isDeath() {
-		if(salud==0) {
+		if (salud == 0) {
 			return true;
 		} else if (!tienealma) {
 			return true;
@@ -48,5 +38,9 @@ public class thewitcher {
 			return false;
 		}
 	}
+		public void recieveDamage(int damage) {
+			salud -= damage;
+			if (salud < 0) salud = 0;
+		}
+	}
 
-}
